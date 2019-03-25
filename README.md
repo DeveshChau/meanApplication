b956573728cb676e8fbd98117edf810dc646a5b2
DeveshChau <deveshachaudhari@gmail.com>
2019-03-25 17:21:32 +0530

    mean post application

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mean Application
+Simple application having CRUD functionality using complete MEAN stack.
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { MatCardModule, MatInputModule, MatButtonModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
